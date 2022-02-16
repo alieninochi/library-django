328e175a935b491adf71590cd909422b6feca1ff
Zoe Holubkova <holy.inochi@gmail.com>
2022-02-16 02:26:27 +0200

    updates in files for terraform and ansible

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -11,11 +11,6 @@ resource "aws_instance" "webserver" {
   }
 }
 
-#resource "aws_key_pair" "webkey" {
-#  key_name = "webkey"
-#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNkHuvf1+Z5GLJUN0UVNr/guhO/P8eeT/frdhB6npZYAG1tCHTQTZl+4R+viSMhos5DMr3rua6Y9dUn3BcxFUFMG0Be7/f6I10tEc4Eff2guv2Dpkm4y4+Xg1gkcAIdExrxjmcHmtxusM3TmBdRCftYnUsbowJxCRp1dGn2js6P7oJHRvbuVqYVsuJgU+UUfjcOT/gnPj8+i+iK73u69fAG9jr6biTCng7gR38udHkP1aXm+w5LE64nlpur9hfAIrOSePKVbkRHKl0bcjbRRkAb8D3I07XOPSaEZBv0T+dyO2wh6xPkYFu6W5Yi7kzSIFdalcsagYvRakVi5wzg+naT19lRDfOd1Qjlm5jPNgdmQ2iaK+pf9E98iiwsdFCU6Zw9GRzcLHn47c7Lo3gfumKFd1Nr6N0dQMaekOA0QTuGjelnj/tu4iURd/1l9oCjhgzO/RI6O33eyznQhx9v0DL8WBeXoawY3R5Hs+omdX29Fn92aFD4jRuU6wug8nOsaU= alieninochi@localdev"
-#}
-
 resource "aws_eip" "ws_ip" {
   vpc = true
   instance = aws_instance.webserver.id
@@ -58,5 +53,5 @@ resource "aws_security_group" "web_rules" {
 }
 
 output "instance_ips" {
-  value = aws_instance.webserver.*.public_ip
+  value = aws_eip.ws_ip.*.public_ip
 }
