b570326aeab1d0df68fcc4fb4b1283f9309a74dd
Zoe Holubkova <holy.inochi@gmail.com>
2022-02-16 14:03:35 +0200

    change instance ip from elastic to dynamic

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "webserver" {
   subnet_id = aws_subnet.ws_subnet.id
   key_name = "webkey"
 
+  associate_public_ip_address = true
+
   tags = {
     Name = "WebServer"
   }
@@ -16,12 +18,12 @@ resource "aws_key_pair" "webkey" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDNkHuvf1+Z5GLJUN0UVNr/guhO/P8eeT/frdhB6npZYAG1tCHTQTZl+4R+viSMhos5DMr3rua6Y9dUn3BcxFUFMG0Be7/f6I10tEc4Eff2guv2Dpkm4y4+Xg1gkcAIdExrxjmcHmtxusM3TmBdRCftYnUsbowJxCRp1dGn2js6P7oJHRvbuVqYVsuJgU+UUfjcOT/gnPj8+i+iK73u69fAG9jr6biTCng7gR38udHkP1aXm+w5LE64nlpur9hfAIrOSePKVbkRHKl0bcjbRRkAb8D3I07XOPSaEZBv0T+dyO2wh6xPkYFu6W5Yi7kzSIFdalcsagYvRakVi5wzg+naT19lRDfOd1Qjlm5jPNgdmQ2iaK+pf9E98iiwsdFCU6Zw9GRzcLHn47c7Lo3gfumKFd1Nr6N0dQMaekOA0QTuGjelnj/tu4iURd/1l9oCjhgzO/RI6O33eyznQhx9v0DL8WBeXoawY3R5Hs+omdX29Fn92aFD4jRuU6wug8nOsaU= alieninochi@localdev"
 }
 
-resource "aws_eip" "ws_ip" {
-  vpc = true
-  instance = aws_instance.webserver.id
-  associate_with_private_ip = "10.0.100.100"
-  depends_on = [aws_internet_gateway.gw]
-}
+#resource "aws_eip" "ws_ip" {
+#  vpc = true
+#  instance = aws_instance.webserver.id
+#  associate_with_private_ip = "10.0.100.100"
+#  depends_on = [aws_internet_gateway.gw]
+#}
 
 resource "aws_security_group" "web_rules" {
   name = "security group for web servers"
@@ -57,6 +59,7 @@ resource "aws_security_group" "web_rules" {
   }
 }
 
-output "instance_ips" {
-  value = aws_eip.ws_ip.*.public_ip
+output "instance_ip" {
+#  value = aws_eip.ws_ip.*.public_ip
+  value = aws_instance.webserver.public_ip
 }
